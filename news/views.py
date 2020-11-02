474d059e78b46c344c7ba2598a978df23b4315f5
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-11-02 10:40:44 -0600

    added a basic 'hello world' qr code image generator

--- news/views.py
+++ news/views.py
@@ -6,21 +6,16 @@ import qrcode
 
 #This function will generate a qr code and save it as an image. Basic fuction for now redirects to CSMP dep
 #  homepage until urls are established for each slide
-def qrGen():
-    siteData = "https://www.missouriwestern.edu/csmp/"
-
-    qrImg = "static/qr.png"
-    img = qrcode.make(siteData)
-    img.save(qrImg)
+def qrGen(request):
+    img = qrcode.make('http://google.com')
+    response = HttpResponse(content_type='image/jpg')
+    img.save(response, "JPEG")
+    return response
 
 
 # Create your views here.
 def index(request):
-    
-    # Generate the qr code, Commented out because it doesn't work. Nothing is generated
-    # qrGen()
 
-    
     print("Passed qrGen()")
     with jsonlines.open('news/static/newsData.jsonl') as reader:
         for obj in reader:
